b6da8d77b72e3d753f7018f3f467585515e74f4f
aviskhatiwada <aviskhatiwada@gmail.com>
2024-11-19 00:57:18 -0500

    New

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,98 +1,87 @@
 #!/usr/bin/python3
-
 import math,random
 
 class SudokuGenerator:
-    def __init__(self, row_len, removed_cells):
-		self.rows=self.cols=self.row_len=self.col_len=row_len
-		self.board= [[0 for _ in range(self.rows)] for _ in range(self.cols)]
-		self.box_len=int(math.sqrt(self.rows))
+    def __init__(self, row_len, rem):
+        self.rows=self.cols=self.row_len=self.col_len=row_len
+        self.board= [[0 for _ in range(self.rows)] for _ in range(self.cols)]
+        self.box_len=int(math.sqrt(self.rows))
+        self.rem=rem
 
-    def get_board(self)->list[list]:
-		# get nums as 2d-array
-        pass
+    def get_board(self)->list[list]: return self.board
 
     def print_board(self):
-        pass
-
+        for r in self.board: print(r)
+        print('------')
 
     def valid_in_row(self, row, num)->bool:
-		# if num in specified row (index) 
-        pass
-	
-    def valid_in_col(self, col, num):
-		# if num in specified col (index) 
-        pass
-
+        return False if num in self.board[row] else True
+    
+    def valid_in_col(self, col, num)->bool:
+        return False if num in [self.board[r][col] for r in range(0,self.row_len)] else True
 
     def valid_in_box(self, row_start, col_start, num):
-		# if num in specified box (index) 
-        pass
-    
+        return False if num in [n for row in [self.board[r][c:c+3] for r,c in zip(range(row_start,row_start+3),(col_start,)*3)] for n in row] else True 
+        #print([self.board[r][c:c+3] for r,c in zip(range(row_start,row_start+3),(col_start,)*3)])
+        #print([n for row in [self.board[r][c:c+3] for r,c in zip(range(row_start,row_start+3),(col_start,)*3)] for n in row])
 
-    def is_valid(self, row, col, num):
-		# will use the above 3 methods
-        pass
-
+    def is_valid(self, row, col, num): 
+        return all((self.valid_in_row(row, num), self.valid_in_col(col,num), self.valid_in_box(row//self.box_len*self.box_len,col//self.box_len*self.box_len,num)))
 
     def fill_box(self, row_start, col_start):
-		rs,re,cs,ce=row_start, row_start+2,col_start,col_start+2
-		for r in range(rs,re):
-			for c in range(cs,ce):
-				self.board[r][c]=random.randint(1,10)
-    
+        rs,re,cs,ce=row_start,row_start+3,col_start,col_start+3
+        for r in range(rs,re):
+            for c in range(cs,ce):
+                while 1:
+                    if self.valid_in_box(rs,cs,n:=random.randint(1,8)): 
+                        self.board[r][c]=n
+                        break
+        #self.print_board()
 
     def fill_diagonal(self):
-		for s in range(0, self.row_len, self.box_len):
-			self.fill_box(s, s)
+        for s in range(0, self.row_len, self.box_len):
+            self.fill_box(s, s)
 
-    '''
-    Should be called after the diagonal boxes have been filled
-	row, col specify the coordinates of the first empty (0) cell
-	returns boolean (whether or not we could solve the board)
-    '''
-    def fill_remaining(self, row, col):
-        if (col >= self.row_length and row < self.row_length - 1):
-            row += 1
-            col = 0
-        if row >= self.row_length and col >= self.row_length:
-            return True
-        if row < self.box_length:
-            if col < self.box_length:
-                col = self.box_length
-        elif row < self.row_length - self.box_length:
-            if col == int(row // self.box_length * self.box_length):
-                col += self.box_length
-        else:
-            if col == self.row_length - self.box_length:
-                row += 1
-                col = 0
-                if row >= self.row_length:
-                    return True
-        
-        for num in range(1, self.row_length + 1):
-            if self.is_valid(row, col, num):
-                self.board[row][col] = num
-                if self.fill_remaining(row, col + 1):
-                    return True
-                self.board[row][col] = 0
+    def fill_remaining(self, r,c):
+        #print("call, ", r,c)
+        if r<self.row_len-1 and c==self.row_len:r,c=r+1,0 #row end
+        if r==self.row_len-1 and c==self.row_len: return True #board end
+        if self.board[r][c]!=0:return self.fill_remaining(r,c+1) #next col
+        if r<self.box_len and c<self.box_len: c=self.box_len #when r,c in first diagonal, c->second box [r,3]
+        elif r<self.row_len-self.box_len and c==r//self.box_len*self.box_len: c+=self.box_len#when r,c in second diagonal, c->third box [r,6]
+        elif r>=self.row_len-self.box_len and c==self.row_len-self.box_len: #last diagonal is already generated, move to next row + base check
+            r,c=r+1,0
+            if r==self.row_len: return True
+        for n in range(1,10):
+            #print(r,c,n)
+            if valid:=self.is_valid(r,c,n):
+                #self.print_board(); print(r,c,n, valid)
+                self.board[r][c]=n
+                if self.fill_remaining(r,c+1): return True
+                self.board[r][c]=0
         return False
 
-
     def fill_values(self):
-	#creates solution
         self.fill_diagonal()
-        self.fill_remaining(0, self.box_length)
+        #self.print_board()
+        self.fill_remaining(0, self.box_len)
 
     def remove_cells(self):
-    #Removes the appropriate number of cells from the board, uses 0 as place-holder
-        pass
+        idx=[]
+        for _ in range(self.rem):
+            while 1:
+                i,j=random.randint(1,8),random.randint(1,8); 
+                if (i,j) not in idx: idx.append((i,j));break
+        for (i,j) in idx: self.board[i][j]=0 
 
-
-def generate_sudoku(size, removed):
-    sudoku = SudokuGenerator(size, removed)
+def generate_sudoku(size=9, rem=10):
+    sudoku = SudokuGenerator(size, rem)
     sudoku.fill_values()
-    board = sudoku.get_board()
     sudoku.remove_cells()
+    sudoku.print_board()
+    '''
+    board = sudoku.get_board()
     board = sudoku.get_board()
     return board
+    '''
+generate_sudoku()
